8ba1161daf5f57e6daff130e272af665b90b0d53
Daniel Shoshan <shush@bigpanda.io>
2014-04-03 11:24:20 +0300

    removed unnecessary comments and log prints

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
-
 var express = require("express");
 var Ansible = require('node-ansible');
 
-var default_inventory = "/etc/ansible/hosts";
+var defaultInventory = "/etc/ansible/hosts";
 var app = express();
 
 app.listen(3000);
 
-// handle command running request
-app.get('/command/:module/:hosts/:args', function(req, res) {
-    var command = new Ansible.AdHoc().module(req.params.module).hosts(req.params.hosts).args(req.params.args).inventory(default_inventory);
+app.get('/command/:module/:hosts/:args', function (req, res) {
+    var command = new Ansible.AdHoc().module(req.params.module).hosts(req.params.hosts).args(req.params.args)
+        .inventory(defaultInventory);
     execute(command, res);
 });
 
-
-// handle playbook running request
-app.get('/playbook/:name*', function(req, res) {
-    var playbook = new Ansible.Playbook().playbook(req.params.name).inventory(default_inventory);
+app.get('/playbook/:name*', function (req, res) {
+    var playbook = new Ansible.Playbook().playbook(req.params.name).inventory(defaultInventory);
     execute(playbook, res);
 });
 
-// execute executable using ansible and inform the user with the result and output
 function execute(exec, res) {
-    exec.exec().then(function(successResult) {
+    exec.exec().then(function (successResult) {
 
         if (successResult.code != 0) {
             res.status(400).send(successResult.output);
-            console.log("1");
             return;
         }
         res.send(successResult.output);
-        console.log("2");
 
-    }, function(error) {
+    }, function (error) {
         res.status(400);
         res.send(error);
-        console.log("3");
     })
 }
